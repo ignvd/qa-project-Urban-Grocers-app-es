c6e3740ddbee19f491bb100e279cb792b4e0f7d0
ignvd <ialarcondiaz2002@gmail.com>
2024-11-20 21:06:36 -0500

    configuration.py

--- configuration.py
+++ configuration.py
@@ -1,4 +1,4 @@
-BASE_URL = "https://cnt-f87e342d-a558-40b7-a0c8-9ef9f746811e.containerhub.tripleten-services.com"
+BASE_URL = "https://cnt-98535ca0-e0a3-4c95-bfbf-38e31bc80ec6.containerhub.tripleten-services.com"
 
 ROUTES = {
     "create_user": "/users",
