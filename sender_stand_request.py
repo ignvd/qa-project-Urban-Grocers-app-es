f456b9e59bb79690013f8011a0ee8f43480c207f
ignvd <ialarcondiaz2002@gmail.com>
2024-11-22 23:29:33 -0500

    sender_stand_request.py

--- sender_stand_request.py
+++ sender_stand_request.py
@@ -1,11 +1,14 @@
 import requests
 from configuration import BASE_URL, ROUTES
+from data import user_body
 
 def create_user():
-    response = requests.post(BASE_URL + ROUTES["create_user"])
+    # Se envía el cuerpo de la solicitud para crear un usuario
+    response = requests.post(BASE_URL + ROUTES["create_user"], json=user_body)
     return response.json()["authToken"]
 
 def create_kit(kit_body, auth_token):
+    # Se asegura que los headers incluyan el token correcto
     headers = {"Authorization": f"Bearer {auth_token}"}
     response = requests.post(BASE_URL + ROUTES["create_kit"], json=kit_body, headers=headers)
     return response
