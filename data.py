96e7c2f6d62e4cb5b833b0de1e1d12f70d5379de
ignvd <ialarcondiaz2002@gmail.com>
2024-11-22 23:28:32 -0500

    data.py

--- data.py
+++ data.py
@@ -1,12 +1,22 @@
 BASE_KIT_BODY = {"name": ""}
 
+Corregido para evitar problemas con librerías y asegurar datos consistentes:
+
+```python
+user_body = {
+    "firstName": "Max",
+    "phone": "+10005553535",
+    "address": "8042 Lancaster Ave.Hamburg, NY"
+}
+
 KIT_BODIES = {
     "valid_min_length": {"name": "a"},
     "valid_max_length": {"name": "a" * 511},
     "too_short": {"name": ""},
     "too_long": {"name": "a" * 512},
     "special_chars": {"name": "№%@,"},
-    "spaces": {"name": " a "},
+    "spaces": {"name": " A Aaa "},
     "numbers": {"name": "123"},
-    "missing_name": {}
+    "missing_name": {},
+    "invalid_type": {"name": 123},
 }
