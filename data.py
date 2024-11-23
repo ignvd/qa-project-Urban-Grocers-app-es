8bb0dc94cc19fe52a25eed3599f276fa9a44d7e1
ignvd <ialarcondiaz2002@gmail.com>
2024-11-22 23:18:31 -0500

    data.py

--- data.py
+++ data.py
@@ -1,12 +1,12 @@
 BASE_KIT_BODY = {"name": ""}
 
 KIT_BODIES = {
-    "valid_min_length": deepcopy(BASE_KIT_BODY),
+    "valid_min_length": {"name": "a"},
     "valid_max_length": {"name": "a" * 511},
     "too_short": {"name": ""},
     "too_long": {"name": "a" * 512},
     "special_chars": {"name": "№%@,"},
-    "spaces": {"name": " A Aaa "},
+    "spaces": {"name": " a "},
     "numbers": {"name": "123"},
     "missing_name": {}
 }
